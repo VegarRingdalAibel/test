0f6a8310347771937e8d0bc590a8ca2c39fd2063
Vegar Ringdal <vegar.ringdal@gmail.com>
2019-08-02 17:37:20 +0200

    update

--- 05f25a027-routes-article-route.js
+++ 05f25a027-routes-article-route.js
@@ -1,11 +1,11 @@
-$fsx.f[57]=(t,e)=>{Object.defineProperty(e,"__esModule",{value:!0});const s=$fsx.r(15);$fsx.r(55);const i=$fsx.r(16),r=$fsx.r(25),a=$fsx.r(56),o=$fsx.r(51),n=$fsx.r(18),c=$fsx.r(47),l=$fsx.r(48),h=$fsx.r(43),m=$fsx.r(49);function d(){return s.html`
+$fsx.f[39]=(t,e)=>{Object.defineProperty(e,"__esModule",{value:!0});const s=$fsx.r(15);$fsx.r(37);const i=$fsx.r(16),r=$fsx.r(25),a=$fsx.r(38),o=$fsx.r(33),n=$fsx.r(18),c=$fsx.r(29),l=$fsx.r(31),h=$fsx.r(30);function m(){return s.html`
         <div class="article-meta">
             <a href="#profile/${this.article.author.username}">
                 <img src.bind="${this.article.author.image?this.article.author.image:""}" />
             </a>
             <div class="info">
                 <a href="#profile/${this.article.author.username}" class="author">${this.article.author.username}</a>
-                <span class="date">${h(new Date(this.article.createdAt),"MMMM D, YYYY")}</span>
+                <span class="date">${t=this.article.createdAt,new Date(t).toLocaleDateString("en",{month:"long",day:"2-digit",year:"numeric"})}</span>
             </div>
             ${this.canModify?s.html`
                       <span>
@@ -36,20 +36,20 @@ $fsx.f[57]=(t,e)=>{Object.defineProperty(e,"__esModule",{value:!0});const s=$fsx
                       </span>
                   `}
         </div>
-    `}i.registerCustomElement("article-comp",class extends HTMLElement{constructor(){super(),this.comments=[],this.articleService=i.getContext(r.ArticleService),this.commentService=i.getContext(a.CommentService),this.userService=i.getContext(o.UserService),this.sharedState=i.getContext(n.SharedState),this.profileService=i.getContext(c.ProfileService)}async activate(t,e,s){this.slug=e.slug,this.article=await this.articleService.get(this.slug),this.comments=await this.commentService.getList(this.slug)||[]}onToggleFavorited(){this.sharedState.isAuthenticated?(this.article.favorited=!this.article.favorited,this.article.favorited?(this.article.favoritesCount++,this.articleService.favorite(this.article.slug)):(this.article.favoritesCount--,this.articleService.unfavorite(this.article.slug)),this.render()):location.hash="login"}onToggleFollowing(){this.sharedState.isAuthenticated?(this.article.author.following=!this.article.author.following,this.article.author.following?this.profileService.follow(this.article.author.username):this.profileService.unfollow(this.article.author.username),this.render()):location.hash="login"}async postComment(){const t=await this.commentService.add(this.slug,this.myComment);this.comments.push(t),this.myComment="",this.render()}get canModify(){return this.article.author.username===this.sharedState.currentUser.username}async deleteArticle(){await this.articleService.destroy(this.article.slug),location.hash="home",this.render()}async deleteComment(t){await this.commentService.destroy(t,this.slug),this.comments=await this.commentService.getList(this.slug),this.render()}markedhtml(t){let e;if(t){const s=new l.Renderer;e=l(t,{renderer:s})}else e="";return e}render(){return console.log("loaded"),s.html`
+    `;var t}i.registerCustomElement("article-comp",class extends HTMLElement{constructor(){super(),this.comments=[],this.articleService=i.getContext(r.ArticleService),this.commentService=i.getContext(a.CommentService),this.userService=i.getContext(o.UserService),this.sharedState=i.getContext(n.SharedState),this.profileService=i.getContext(c.ProfileService)}async activate(t,e,s){this.slug=e.slug,this.article=await this.articleService.get(this.slug),this.comments=await this.commentService.getList(this.slug)||[]}onToggleFavorited(){this.sharedState.isAuthenticated?(this.article.favorited=!this.article.favorited,this.article.favorited?(this.article.favoritesCount++,this.articleService.favorite(this.article.slug)):(this.article.favoritesCount--,this.articleService.unfavorite(this.article.slug)),this.render()):location.hash="login"}onToggleFollowing(){this.sharedState.isAuthenticated?(this.article.author.following=!this.article.author.following,this.article.author.following?this.profileService.follow(this.article.author.username):this.profileService.unfollow(this.article.author.username),this.render()):location.hash="login"}async postComment(){const t=await this.commentService.add(this.slug,this.myComment);this.comments.push(t),this.myComment="",this.render()}get canModify(){return this.article.author.username===this.sharedState.currentUser.username}async deleteArticle(){await this.articleService.destroy(this.article.slug),location.hash="home",this.render()}async deleteComment(t){await this.commentService.destroy(t,this.slug),this.comments=await this.commentService.getList(this.slug),this.render()}markedhtml(t){let e;if(t){const s=new h.Renderer;e=h(t,{renderer:s})}else e="";return e}render(){return console.log("loaded"),s.html`
                 <div class="article-page">
                     <div class="banner">
                         <div class="container">
                             <h1>${this.article.title}</h1>
                             <!--   just to stop repeating myself -->
-                            ${d.call(this)}
+                            ${m.call(this)}
                         </div>
                     </div>
 
                     <div class="container page">
                         <div class="row article-content">
                             <div class="col-md-12">
-                                ${m.unsafeHTML(this.markedhtml(this.article.body))}
+                                ${l.unsafeHTML(this.markedhtml(this.article.body))}
                             </div>
                         </div>
 
@@ -57,7 +57,7 @@ $fsx.f[57]=(t,e)=>{Object.defineProperty(e,"__esModule",{value:!0});const s=$fsx
 
                         <div class="article-actions">
                             <!--   just to stop repeating myself -->
-                            ${d.call(this)}
+                            ${m.call(this)}
                         </div>
 
                         <div class="row">
@@ -96,7 +96,7 @@ $fsx.f[57]=(t,e)=>{Object.defineProperty(e,"__esModule",{value:!0});const s=$fsx
                         </div>
                     </div>
                 </div>
-            `}})},$fsx.f[55]=(t,e)=>{Object.defineProperty(e,"__esModule",{value:!0});const s=$fsx.r(15),i=$fsx.r(16),r=$fsx.r(18),a=$fsx.r(4),o=$fsx.r(43);i.registerCustomElement("comment-section",class extends HTMLElement{get canModify(){return this.comment.author.username===this.sharedState.currentUser.username}deleteCb(t){}constructor(){super(),this.sharedState=i.getContext(r.SharedState)}render(){return s.html`
+            `}})},$fsx.f[37]=(t,e)=>{Object.defineProperty(e,"__esModule",{value:!0});const s=$fsx.r(15),i=$fsx.r(16),r=$fsx.r(18),a=$fsx.r(4);i.registerCustomElement("comment-section",class extends HTMLElement{get canModify(){return this.comment.author.username===this.sharedState.currentUser.username}deleteCb(t){}constructor(){super(),this.sharedState=i.getContext(r.SharedState)}render(){return s.html`
                 <div class="card">
                     <div class="card-block">
                         <p class="card-text">${this.comment.body}</p>
@@ -111,11 +111,11 @@ $fsx.f[57]=(t,e)=>{Object.defineProperty(e,"__esModule",{value:!0});const s=$fsx
 
                         <a href="${"#profile/"+this.comment.author.username}" class="comment-author">${this.comment.author.username}</a>
 
-                        <span class="date-posted">${o(new Date(this.comment.createdAt),"MMMM D, YYYY")}</span>
+                        <span class="date-posted">${t=this.comment.createdAt,new Date(t).toLocaleDateString("en",{month:"long",day:"2-digit",year:"numeric"})}</span>
                         ${this.canModify?s.html` <span class="mod-options">
                             <i class="ion-trash-a" @click=${()=>{this.deleteCb(this.comment.id)}}></i>
                         </span>`:""}
                        
                     </div>
                 </div>
-            `}})},$fsx.f[56]=(t,e)=>{Object.defineProperty(e,"__esModule",{value:!0});const s=$fsx.r(23),i=$fsx.r(16);e.CommentService=class{constructor(){this.apiService=i.getContext(s.ApiService)}async add(t,e){return(await this.apiService.post(`/articles/${t}/comments`,{comment:{body:e}})).comment}async getList(t){return(await this.apiService.get(`/articles/${t}/comments`)).comments}async destroy(t,e){return await this.apiService.delete(`/articles/${e}/comments/${t}`)}}};
+            `;var t}})},$fsx.f[38]=(t,e)=>{Object.defineProperty(e,"__esModule",{value:!0});const s=$fsx.r(23),i=$fsx.r(16);e.CommentService=class{constructor(){this.apiService=i.getContext(s.ApiService)}async add(t,e){return(await this.apiService.post(`/articles/${t}/comments`,{comment:{body:e}})).comment}async getList(t){return(await this.apiService.get(`/articles/${t}/comments`)).comments}async destroy(t,e){return await this.apiService.delete(`/articles/${e}/comments/${t}`)}}};
